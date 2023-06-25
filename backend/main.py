4681cab2d577f95b42579a06da84a31016bc511d
Vlad Grigoriev <vgrig2006@gmail.com>
2023-06-25 09:54:29 +0700

    create backend files

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ def view():
     file = request.files['file']
     file.save(file.filename)
     data = pd.read_excel(file)
-    return  data.to_html()
+    return data.to_html()
 
 
 app.run('0.0.0.0', port=5000, debug=True)
