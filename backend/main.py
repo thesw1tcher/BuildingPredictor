2e35355130df74ee0dbe00721099b6a33a2c8c3d
Vlad Grigoriev <vgrig2006@gmail.com>
2023-06-25 11:43:36 +0700

    backend files

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_file
 import pandas as pd
 from catboost import CatBoostRegressor
+from time import time
 
 
 app = Flask(__name__)
@@ -14,12 +15,65 @@ def upload():
 def view():
     file = request.files['file']
     file.save(file.filename)
+
     data = pd.read_excel(file)
+    attr = pd.read_csv('./stat/attr.csv')
+
+    area = []
+    for el in data.obj_key:
+        area.append(attr[attr['obj_key'] == el]['Площадь'].mean())
+    data['area'] = area
+    data['area'] = data['area'].fillna(attr['Площадь'].mean())
+
+    date = data['ДатаНачалаЗадачи']
+
+    season = []
+
+    for i in range(len(date)):
+        a = date[i]
+        pred = a[5:7]
+        if pred in ['01', '02', '12']:
+            season.append('Зима')
+        elif pred in ['03', '04', '05']:
+            season.append('Весна')
+        elif pred in ['06', '07', '08']:
+            season.append('Лето')
+        else:
+            season.append('Осень')
+
+    data['ВремяГодаНачала'] = season
+
+    date = data['ДатаокончанияБП0']
+
+    season = []
+
+    for i in range(len(date)):
+        a = date[i]
+        pred = a[5:7]
+        if pred in ['01', '02', '12']:
+            season.append('Зима')
+        elif pred in ['03', '04', '05']:
+            season.append('Весна')
+        elif pred in ['06', '07', '08']:
+            season.append('Лето')
+        else:
+            season.append('Осень')
+
+    data['ВремяГодаОкончанияБП0'] = season
+    data = data.drop(['ДатаНачалаЗадачи', 'ДатаОкончанияЗадачи', 'ДатаначалаБП0', 'ДатаокончанияБП0'], axis=1)
+
+
 
     model = CatBoostRegressor()
     model.load_model('./model/model.cbm', format='cbm')
 
-    return send_from_directory(directory='./backend', filename=file.filename)
+    pred = pd.DataFrame(model.predict(data), columns=['prediction'])
+
+    name = f'./predictions_file/predictions{int(time())}'
+
+    pred.save(name)
+
+    return send_file(path_or_file=name, as_attachment=True)
 
 
 app.run('0.0.0.0', port=5000, debug=True)
